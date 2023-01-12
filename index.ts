0111bcf70aaf8602ebd1c2625d7c32c6afba5bf4
Tristan F <LeoDog896@hotmail.com>
2023-01-12 13:25:49 +0000

    refactor: use function declaration

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ let sequenceNumber = 0;
  *
  * Uses a buffer if available, falls back to crypto.randomBytes
  */
-export const getRandomBytes = function (bytes: number) {
+export function getRandomBytes (bytes: number) {
   const BUFFER_SIZE = 4096;
 
   bytes = bytes || 12;
@@ -71,7 +71,7 @@ export const getRandomBytes = function (bytes: number) {
  *
  * (Original version from socket.io <http://socket.io>)
  */
-export const generateId = function () {
+export function generateId () {
   const rand = Buffer.alloc(15); // multiple of 3 for base64
   if (!rand.writeInt32BE) {
     return Math.abs(Math.random() * Math.random() * Date.now() | 0).toString() +
